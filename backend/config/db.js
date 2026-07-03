aabf7eb1b3847bd9277bfee00dd9fc25ab638594
Manoj <manojsaravanan723@gmail.com>
2026-07-03 20:26:03 +0530

    Clean Git tracking and update backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    console.log(process.env.MONGO_URI);
+    //console.log(process.env.MONGO_URI);
 
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
